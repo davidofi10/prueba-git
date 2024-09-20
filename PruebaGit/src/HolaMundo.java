b7c19d64b7eb5fcab55f9b4dd5b2e5912c389a47
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-09-20 08:37:37 +0200

    de

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -3,6 +3,6 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Adios mundo !!!");
-		System.out.println("Esto es otro mensaje");
+		System.out.println("EJHFJDDJKFHJDSFJK");
 	}
 }
